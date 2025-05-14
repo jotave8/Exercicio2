7fe5fb1b64d0b3c18375b4313606b852556aeb9b
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-05-14 13:05:03 +0000

    update

--- Member.java
+++ Member.java
@@ -1,1 +1,36 @@
 
+import java.util.ArrayList;
+
+class Member{
+    private String nome;
+    private int id;
+    private String email;
+    private static userID =0;
+    private ArrayList<Livro> historicoLeitura;
+
+    public Membro(int id){
+        this.id = userID++;
+        this.historicoLeitura = new ArrayList<>();
+    }
+
+    public getNome(){
+        return nome;
+    }
+
+    public setNome(String nome){
+        this.nome = nome;
+    }
+
+    public getId(int id){
+        return id;
+    }
+
+    public getEmail (String email){
+        return email;
+    }
+
+    public setEmail (String email){
+        this.email = email;
+    }
+
+}
