4e776c5229753249b85cc990bf67bc0813878bb8
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-05-14 18:34:45 +0000

    update

--- Book.java
+++ Book.java
@@ -1,32 +1,39 @@
-class Livro{
-    private String nome;
-    private String autor;
-    private String genero;
+class Book{
+    private String title;
+    private String author;
+    private String gender;
+    private int year;
     private int id;
     private static int bookID = 0;
 
-    public  Livro(int id){
+    public Book(int id){
         this.id = bookID++;
     }
 
-    public getNome(){
-        return nome;
+    public getTitle(){
+        return title;
     }
 
-    public setNome(String nome){
-        this.nome = nome;
+    public setNome(String title){
+        this.title = title;
     }
 
     public getId(int id){
         return id;
     }
 
-    public getAutor (String autor){
-        return autor;
+    public getAuthor (String author){
+        return author;
     }
 
-    public setAutor (String autor){
-        this.autor = autor;
+    public setAuthor (String author){
+        this.author = author;
     }
 
+    public getYear (int year){
+        return year;
+    }
+
+    public setYear (int year){
+        this.year = year;
 }
